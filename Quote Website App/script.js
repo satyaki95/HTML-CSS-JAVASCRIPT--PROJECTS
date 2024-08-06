cc47ad9961875bd7f4ec173f9ea1c8203351a255
satyaki95 <www.satyaki95@gmail.com>
2024-08-06 14:16:24 +0530

    quote

---
+++ Quote Website App/script.js
@@ -0,0 +1,22 @@
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+const api_url = "https://api.quotable.io/random";
+
+async function getQuote(url) {
+  const response = await fetch(url);
+  var data = await response.json();
+
+  quote.innerHTML = data.content;
+  author.innerHTML = data.author;
+}
+
+function tweet() {
+  window.open(
+    "http://twitter.com/intent/tweet?text=" +
+      quote.innerHTML +
+      "--- by" +
+      author.innerHTML,
+    "Tweet Window",
+    "width=600, height=300"
+  );
+}
