871090c96fe266d6c83eb93bcb11c121f49249e4
satyaki95 <www.satyaki95@gmail.com>
2024-08-19 17:55:37 +0530

    header

---
+++ Food Delivary App/src/main.js
@@ -0,0 +1,31 @@
+/*~~~~~~~~~~~~~~~ TOGGLE BUTTON ~~~~~~~~~~~~~~~*/
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+const closeIcon = document.getElementById("nav-close");
+const navLinks = document.querySelectorAll(".nav_link");
+
+hamburger.addEventListener("click", () => {
+  navMenu.classList.remove("hidden");
+});
+
+closeIcon.addEventListener("click", () => {
+    navMenu.classList.add("hidden");
+})
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        navMenu.classList.add("hidden");
+        })
+})
+
+/*~~~~~~~~~~~~~~~ TABS ~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~ DARK LIGHT THEME ~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~ SCROLL SECTIONS ACTIVE LINK ~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~ SCROLL REVEAL ANIMATION ~~~~~~~~~~~~~~~*/
