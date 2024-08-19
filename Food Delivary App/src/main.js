2b7851463e22bf69e066dc016619650a4646d6cd
satyaki95 <www.satyaki95@gmail.com>
2024-08-19 23:04:24 +0530

    Menu

--- Food Delivary App/src/main.js
+++ Food Delivary App/src/main.js
@@ -20,11 +20,41 @@ navLinks.forEach((link) => {
 
 /*~~~~~~~~~~~~~~~ TABS ~~~~~~~~~~~~~~~*/
 const tabs = document.querySelectorAll(".tabs_wrap ul li");
+const all = document.querySelectorAll(".item_wrap");
+const foods = document.querySelectorAll(".food");
+const snacks = document.querySelectorAll(".snack");
+const beverages = document.querySelectorAll(".beverage");
 
 tabs.forEach((tab) => {
   tab.addEventListener("click", () => {
-    tabs.forEach((t) => t.classList.remove("active"));
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
     tab.classList.add("active");
+
+    const tabval = tab.getAttribute("data-tabs");
+
+    all.forEach((item) => {
+      item.style.display = "none";
+    });
+
+    if (tabval == "food") {
+      foods.forEach((item) => {
+        item.style.display = "block";
+      });
+    } else if (tabval == "snack") {
+      snacks.forEach((item) => {
+        item.style.display = "block";
+      });
+    } else if (tabval == "beverage") {
+      beverages.forEach((item) => {
+        item.style.display = "block";
+      });
+    } else {
+      all.forEach((item) => {
+        item.style.display = "block";
+      });
+    }
   });
 });
 
